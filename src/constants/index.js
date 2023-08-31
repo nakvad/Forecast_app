1ad85f43dd15d2e71a1562c2f344dada60b435b9
Evgeniy Busin <evgeny.busin@gmail.com>
2023-08-31 19:58:33 +0300

    add info in constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
 export const API_KEY = '3e6910b169bd3833c92b4ce170f7fc9c'
-export const BASE_URL = '3e6910b169bd3833c92b4ce170f7fc9c'
+export const BASE_URL = 'https://api.openweathermap.org/data/3.0/onecall'
